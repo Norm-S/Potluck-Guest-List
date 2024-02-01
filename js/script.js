3b64f1ccea3d5e774471e5256bf3528185356465
shum_n <shumnorman@gmail.com>
2024-01-31 19:50:43 -0800

    Add 2 variables & create assignItems()

--- js/script.js
+++ js/script.js
@@ -11,6 +11,9 @@ const guestCount = document.querySelector(".attendance");
 // alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
 
+const assignButton = document.querySelector(".assign");
+const assignedItems = document.querySelector(".assigned-items");
+
 addGuestButton.addEventListener("click", function () {
   const guest = guestInput.value;
   //   console.log(guest);
@@ -42,4 +45,21 @@ const updateGuestCount = function () {
     guestInputLabel.classList.add("hide");
     guestFull.classList.remove("hide");
   }
+};
+
+const assignItems = function () {
+  const potluckItems = [
+    "potato salad",
+    "cookies",
+    "fruit salad",
+    "pesto pasta",
+    "cheesecake",
+    "gazpacho",
+    "spaghetti",
+    "sliders",
+    "garlic bread",
+    "fried rice",
+    "cobbler",
+    "quiche",
+  ];
 };
